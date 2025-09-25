94e10fa8af3ff7b448d8287deacbf58c10e29b10
Shweta <shwetanarkhede52@gmail.com>
2025-09-25 21:33:46 +0530

    commit changes

---
+++ Day1Hibernate/src/main/java/com/main/MainClass.java
@@ -0,0 +1,38 @@
+package com.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.entity.Student;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Student.class);
+
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tr = ss.beginTransaction();
+
+		Student s = new Student();
+		s.setStud_id(1);
+		s.setName("shweta");
+		s.setAddress("pune");
+		s.setAge(22);
+
+
+		ss.persist(s);
+
+		System.out.println("Data is inserted...");
+
+		tr.commit();
+		ss.close();
+
+	}
+
+}
